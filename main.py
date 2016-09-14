26498f7bf460eeb51e5b4f3417d80d56254e7966
jgeofil <jeremy.geo@gmail.com>
2016-09-15 01:07:40 +0200

    clean data

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import pandas as pd
-f = pd.read_csv("Project1_data.csv", low_memory=False)
-print(f)
+dataframe = pd.read_csv("Project1_data.csv", low_memory=False)
+ids = dataframe['PARTICIPANT ID']
+numCols = len(dataframe.columns)
+colNames = dataframe.ix[:,0:6].columns.values
+
+for i in range(1,(numCols/6)+14):
+    dataframe.insert((i*6), 'PARTICIPANT ID'+str(i), ids)
+
+numCols = len(dataframe.columns)
+
+nf = None;
+for i in range(0, numCols/6):
+    sub = dataframe.ix[:,i*6:(i*6)+6]
+    sub.columns = colNames
+    if(nf is None):
+        nf = sub
+    else:
+        nf = nf.append(sub)
+
+nf = nf[pd.notnull(nf['EVENT DATE'])]
+
+nf.to_csv('out.csv')
+
+print(nf)
