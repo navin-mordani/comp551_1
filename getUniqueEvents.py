83d8f4478991791573c8ab0667c22977a43e9d3c
jaspal1329 <jaspal13.007@gmail.com>
2016-09-17 23:16:56 -0400

    Add files via upload

--- getUniqueEvents.py
+++ getUniqueEvents.py
@@ -3,6 +3,14 @@ import numpy as np
 from datetime import datetime
 
 nf = pd.read_csv("out.csv", low_memory=False)
+col = ['PARTICIPANT ID', 'GENDER', 'TIME IF COMPLETING A MARATHON', 'AGE']
+
+def timeToSeconds (time):
+    if time == '-1':
+        return 6*3600
+    else:
+        d=datetime.strptime( time, '%H:%M:%S')
+        return d.hour*3600 + d.minute*60 + d.second
 
 eventTypes = nf.ix[:,3:4]
 eventTypes = eventTypes.drop_duplicates()
